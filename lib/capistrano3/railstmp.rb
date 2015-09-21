a93c7955dc4848fb0470573fd02e298043550ba4
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2015-09-21 16:21:19 +0900

    で作った状態です。.gitignore はちょっと変更した。

---
+++ lib/capistrano3/railstmp.rb
@@ -0,0 +1,7 @@
+require "capistrano3/railstmp/version"
+
+module Capistrano3
+  module Railstmp
+    # Your code goes here...
+  end
+end
---
+++ spec/capistrano3/railstmp_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Capistrano3::Railstmp do
+  it 'has a version number' do
+    expect(Capistrano3::Railstmp::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
